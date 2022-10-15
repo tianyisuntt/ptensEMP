2f5e70702d976ddf396b47aade754aadb7d9e26b
Andrew Hands <andrewrhands@gmail.com>
2022-10-15 13:24:58 -0500

    fixed issue with weights

--- GCNConv_Tester.py
+++ GCNConv_Tester.py
@@ -1,7 +1,6 @@
-from sqlite3 import paramstyle
-from turtle import forward
 from typing import List
 import torch
+import numpy as np
 from ptens_layers import GCNConv, Linear
 #from torch_geometric.nn import GCNConv as PyG_GCNConv, Sequential
 from torch_geometric.nn import GCNConv as PyG_GCNConv
@@ -13,7 +12,7 @@ from torch.nn import EmbeddingBag, ReLU, Softmax
 import ptens
 # hyper parameters
 use_pyg_model = False
-learning_rate = 0.001
+learning_rate = 0.00001
 decay = 0.5
 epochs = 20
 warmup_epochs = 10
@@ -59,30 +58,32 @@ class Model(torch.nn.Module):
   def __init__(self) -> None:
     super().__init__()
     self.embedding = EmbeddingBag(in_channels,128)
-    self.conv1 = GCNConv(128,128,False)
-    self.conv2 = GCNConv(128,128,False)
+    self.conv = [
+      GCNConv(128,128,False)
+      for i in range(5)
+    ]
     self.lin = Linear(128,out_channels,False)
   def parameters(self, recurse: bool = True) -> List[torch.nn.Parameter]:
     params = []
     params += self.embedding.parameters()
-    params += self.conv1.parameters()
-    params += self.conv2.parameters()
+    for c in self.conv:
+      params += c.parameters()
     params += self.lin.parameters()
     return params
+  def train(self, mode: bool = True):
+    self.embedding.train(mode)
+    [c.train(mode) for c in self.conv]
+    self.lin.train(mode)
+    return super().train(mode)
   def forward(self, x: torch.Tensor, G: ptens.graph) -> torch.Tensor:
-    #print(list(self.embedding.parameters())[0].flatten()[0])
     x = self.embedding(x)
     x = ptens.ptensors0.from_matrix(x)
     x = ptens.relu(x)
-    x = self.conv1(x,G)
-    x = ptens.relu(x)
-    x = self.conv2(x,G)
+    for c in self.conv:
+      x = ptens.relu(c(x,G))
     x = self.lin(x)
-    print(list(self.lin.parameters())[0].flatten()[0])
     x = x.torch()
-    #x = x.sum()
-    #x.backward()
-    #x = torch.softmax(x,1)
+    x = torch.softmax(x,1)
     return x
 model = Model()
 print([s.size() for s in model.parameters()])
@@ -90,19 +91,46 @@ print([s.size() for s in model.parameters()])
 # running tests
 def compute_accuracy(mask: torch.Tensor) -> float:
   with torch.no_grad():
-    predictions = model(x,G)[mask].detach().argmax(1)
+    predictions = model(x,G)[mask].detach()
+    print(predictions)
+    predictions = predictions.argmax(1)
   labels = y[mask].detach()
+  print(predictions)
+  print(labels)
+  raise Exception()
   return (predictions == labels).float().mean()
 
 loss = torch.nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
 schedular = torch.optim.lr_scheduler.ExponentialLR(optimizer,decay)
 train_y = y[train_mask]
+model.train()
+init_weights = [p + 0 for p in model.parameters()]
+val_history = []
+train_history = []
+test_history = []
 for epoch in range(epochs):
+  print("\nepoch: %d" % (epoch + 1))
   optimizer.zero_grad()
   l = loss(model(x,G)[train_mask],train_y)
-  #print("loss:",l)
+  print("loss:",l)
   l.backward()
+  #print(list(model.parameters())[-1].grad)
   optimizer.step()
   if epoch >= warmup_epochs:
     schedular.step()
+  #weights = [p + 0 for p in model.parameters()]
+  #same = [(init_weights[i] == weights[i]).all() for i in range(len(weights))]
+  #print(np.argwhere(same))
+  val_history.append(compute_accuracy(val_mask).tolist())
+  train_history.append(compute_accuracy(train_mask).tolist())
+  test_history.append(compute_accuracy(test_mask).tolist())
+  print("val accuracy:",val_history[-1])
+  print("train accuracy:",train_history[-1])
+  print("test accuracy:",test_history[-1])
+from matplotlib import pyplot as plt
+plt.plot(train_history)
+plt.plot(test_history)
+plt.plot(val_history)
+plt.legend(labels=['train','test','val'])
+plt.savefig('train_history.png')
