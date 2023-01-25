c3837f93f50fc2418fb4d506dd23b121af10e3bb
Tianyi Sun <sun00234@umn.edu>
2023-01-25 16:45:23 -0600

    updates

---
+++ Cora_P1GCNmodel2.py
@@ -0,0 +1,76 @@
+import torch
+import ptens
+from torch.nn.functional import softmax, dropout
+from torch_geometric.datasets import Planetoid
+from torch_geometric.transforms import NormalizeFeatures
+from torch_geometric.transforms.random_node_split import RandomNodeSplit
+from Transforms import ToPtens_Batch
+dataset = Planetoid(root='data/Planetoid', name='Cora', transform=NormalizeFeatures())
+data = dataset[0] 
+transform_nodes = RandomNodeSplit(split = 'train_rest',
+                                  num_val = 433,
+                                  num_test = 540)
+data = transform_nodes(data)
+on_learn_transform = ToPtens_Batch()
+data = on_learn_transform(data)
+
+class P1GCN(torch.nn.Module):
+    def __init__(self, hidden_channels, reduction_type):
+        super().__init__()
+        torch.manual_seed(12345)
+        self.conv1 = ptens.modules.ConvolutionalLayer_1P(dataset.num_features, hidden_channels, reduction_type)
+        self.conv2 = ptens.modules.ConvolutionalLayer_1P(hidden_channels, dataset.num_classes, reduction_type)
+        self.dropout = ptens.modules.Dropout(prob=0.5,device = None)
+
+    def forward(self, x, edge_index):
+        x = ptens.linmaps1(x, False)
+        x = self.conv1(x,edge_index).relu()
+        x = ptens.linmaps0(x, False).torch()
+        x = softmax(x, dim = 1)
+        x = ptens.ptensors0.from_matrix(x)
+        x = self.dropout(x)  
+
+        x = ptens.linmaps1(x, False)  
+        x = self.conv2(x,edge_index).relu()
+        x = ptens.linmaps0(x, False).torch()
+        x = softmax(x, dim = 1)
+        x = ptens.ptensors0.from_matrix(x)
+        x = ptens.linmaps1(x, False) 
+        x = self.dropout(x) 
+        
+        x = ptens.linmaps0(x, False)
+        return x
+
+
+def train():
+      model.train()
+      optimizer.zero_grad()
+      data_x = ptens.ptensors0.from_matrix(data.x)
+      out = model(data_x,data.G).torch()
+      loss = criterion(out[data.train_mask], data.y[data.train_mask])  
+      loss.backward() 
+      optimizer.step() 
+      return loss
+def test():
+      model.eval()
+      data_x = ptens.ptensors0.from_matrix(data.x)
+      out = model(data_x,data.G).torch()
+      pred = out.argmax(dim=1)  
+      train_correct = pred[data.train_mask] == data.y[data.train_mask]  
+      train_acc = int(train_correct.sum()) / int(data.train_mask.sum())  
+      test_correct = pred[data.test_mask] == data.y[data.test_mask]  
+      test_acc = int(test_correct.sum()) / int(data.test_mask.sum()) 
+      return train_acc, test_acc
+
+    
+model = P1GCN(hidden_channels = 32, reduction_type = "mean") 
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=8e-4)
+criterion = torch.nn.CrossEntropyLoss()
+for epoch in range(1, 201):
+    loss = train()
+    train_acc, test_acc = test()
+    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}')
+    print(f'Train acc:{train_acc:.4f}, Test acc:{test_acc:.4f}')
+print("=================================================================")
+# Epoch: 192, Loss: 1.9435
+# Train acc:0.5167, Test acc: 0.4991
