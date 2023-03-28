7a81d64ef53d7d74ef8654783bce28bdddd8142c
Tianyi Sun <sun00234@umn.edu>
2023-03-28 15:33:34 -0500

    updates

--- GTL0.py
+++ GTL0.py
@@ -3,6 +3,48 @@ import torch
 import ptens
 import torch.nn as nn
 
+def guassian_kernel(source, target, kernel_mul = 2.0, kernel = 5, fix_sigma = None):
+    n_samples = int(source.size()[0])+int(target.size()[0])
+    tot = torch.cat([source, target],dim = 0)
+    tot0 = torch.cat([source, target],dim = 0)
+    tot1 = tot.unsqueeze(0).expand(int(tot.size(0)),int(tot.size(0)),int(tot.size(1)))
+    L2_distance = ((tot0-tot1)**2).sum(2)
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data)/(n_samples**2-n_samples)
+    bandwidth /= kernel_mul ** (kernel_num//2)
+    bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+    kernel_val = [torch.exp(-L2_distance/bandwidth_temp) for bandwidth_temp in bandwidth_list]
+    return sum(kernel_val)
+
+def mmd_rbf_acc(source, target, kernel_mul = 2.0, kernel_num = 5, fix_sigma = None):
+    batch_size = int(source.size()[0])
+    kernels = gaussian_kernel(source, target,
+                              kernel_mul = kernel_mul,
+                              kernel_num = kernel_num,
+                              fix_sigma = fix_sigma)
+    loss = 0
+    for i in range(batch_size):
+        s1, s2 = i, (i+1)%batch_size
+        t1, t2 = s1+batch_size, s2+batch_size
+        loss += kernels[s1, s2] + kernels[t1, t2]
+        loss -= kernels[s1, t2] + kernels[s2, t1]
+    return loss / float(batch_size)
+
+def mmd_rbf_noacc(source, target, kernel_mul = 2.0, kernel_num = 5, fix_sigma = None):
+    batch_size = int(source.size()[0])
+    kernels = gaussian_kernel(source, target,
+                              kernel_mul = kernel_mul,
+                              kernel_num = kernel_num,
+                              fix_sigma = fix_sigma)
+    XX = kernels[:batch_size, :batch_size]
+    YY = kernels[batch_size:, batch_size:]
+    XY = kernels[:batch_size, batch_size:]
+    YX = kernels[batch_size:, :batch_size]
+    loss = torch.mean(XX+YY-XY-YX)
+    return loss
+
 def conv3x3(in_p, out_p, stride = 1, groups = 1):
     return nn.Conv2d(in_p, out_p, kernel_size = 3,
                      stride = stride, padding = 1, bias = False)
@@ -69,14 +111,16 @@ class BN(nn.Module):
         return out
 
 class RN(nn.Module):
-    def __init__(self, block, layers, n_classes = 1000, zero_init_res = False, groups = 1, width_per_group = 64, norm_layer = None):
+    def __init__(self, block, layers, n_classes = 1000, zero_init_res = False,
+                 groups = 1, width_per_group = 64, norm_layer = None):
         super(RN, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         self.in_p = 64
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.in_p, kernel_size = 7, stride = 2, padding = 3, bias = False)
+        self.conv1 = nn.Conv2d(3, self.in_p, kernel_size = 7,
+                               stride = 2, padding = 3, bias = False)
         self.bn1 = norm_layer(self.in_p)
         self.relu = nn.ReLU(inplace = True)
         self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
@@ -122,7 +166,6 @@ class RN(nn.Module):
             x = self.bn1(x)
             x = self.relu(x)
             x = self.maxpool(x)
-
             x = self.layer1(x)
             x = self.layer2(x)
             x = self.layer3(x)
